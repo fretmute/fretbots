07db740581d34549c263485fb2eb1937fc422836
Jason <fretmute@gmail.com>
2022-06-24 22:08:32 -0500

    0.6.2.0
    Merged PR#15. Rescaled timings for bot neutral items.

--- vscripts/Version.lua
+++ vscripts/Version.lua
@@ -1,32 +1,4 @@
-version = '0.6.1.6'
+version = '0.6.2.0'
 versionString = [[Changelog:
-	Added common name for Dawnbreaker. Evidently at some point I also added
-	Hoodwink and did not mention it.
-	
-	Added options and methods for punishing cheaters.  At the moment this will 
-	kill them if they are alive (or when they become alive) three times per
-	cheat. This is mostly due to the fact that the cheaters into which I've run
-	typically use -refresh and -respawn.  If they start giving themselves XP
-	or gold I'll have to be less lazy.
-	
-	Properly tracking isEnableCheatRepurcussions now.  Whoops!
-	
-	Updated neutral item list to be current.  Bot item desires for new items
-	are complete crapshoots at this point.	
-	
-	Updated bot neutral item drop count to five per tier.
-	
-	Added 'getroles' chat command to dump bot positions to chat. 
-	Added bot skill print to RoleDetermination:AnnounceRoles().
-	
-	Typo fix in in Utilities right at the top from dashinja. Thanks for the help!
-	
-	Typo fix of the word Utilities in the last typo fix.  
-	Super meta.  Should probably not commit on Friday nights.
-	
-	Added some of the 2021 TI announcer voicelines.
-	
-	Changed playsound such that it uses string.upper on the argument so folks
-	don't have to type their sounds in all caps.  They still, of course, have
-	to know exactly what table entry they want.
+	Merged PR#15. Rescaled timings for bot neutral items.
 ]]
