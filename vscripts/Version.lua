80806dc3a1d4b37248916862f295100caf906059
Jason <fretmute@gmail.com>
2022-08-23 19:47:35 -0500

    0.6.3.1
    Adjusted SettingsNeutralItemTable.lua to reflect changes from patch 7.32.

--- vscripts/Version.lua
+++ vscripts/Version.lua
@@ -1,8 +1,10 @@
-version = '0.6.3.0'
+version = '0.6.3.1'
 versionString = [[Changelog:
   Attempted to fix the dependency on DataTables.difficultyScale inside of the
   Fretbots:Initialize() that was introduced with the neutral timing change
   from the last patch.  
   
   Merged PR#15. Rescaled timings for bot neutral items.
+  
+  Adjusted SettingsNeutralItemTable.lua to reflect changes from patch 7.32.
 ]]
