41868ffece29acfa9c47a2b3d29cb7a38879b8ae
Jason Cross <fretmute@gmail.com>
2024-11-13 00:47:48 -0600

    0.7.2.1
    Added Ringmaster and Kez to HeroNames.lua.
    
    This is completely untested and I'm uploading it based on mark one eyeballs on the changes, so if it breaks stuff, I apologize.

--- vscripts/FretBots/Version.lua
+++ vscripts/FretBots/Version.lua
@@ -1,5 +1,7 @@
-version = '0.7.2.0'
-versionString = [[Changelog: 
+version = '0.7.2.1'
+versionString = [[Changelog:
+  Added Ringmaster and Kez to HeroNames.lua.
+
   Updated neutral items for 7.37.
 
   Added Primal Beast to HeroNames.lua.
